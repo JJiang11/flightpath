1efaa9f76b5e59a783f9f5c57ce9d4a3c4ddcb0f
AdrianDP1 <63573880+AdrianDP1@users.noreply.github.com>
2020-06-14 18:23:18 -0500

    update plot map

--- R/plot_map.R
+++ R/plot_map.R
@@ -3,22 +3,54 @@
 #' @description Creates and returns an interactive map object displaying plane's path and chosed variable information.
 #' @param data sf of linestrings and interpolated nonspatial data.
 #' @param variable String of variable that user wants to respresent by color on the linestrings.
+#' @param view Boolean (default TRUE) that specifies if the map returned should be an interactive view map
+#' or a stationary plot map, the latter of which conains cartographic elements.
 #'
-#' @return Interactive tmap map of plane path on top of an OpenTopoMap basemap
+#' @return tmap of plane path on top of an OpenTopoMap basemap
 #'
 #' @examples
 #' \dontrun{plot_map(data, "velocity")}
 #'
 #' @import tmap
 #' @import sf
+#' @import plyr
 #' @import leaflet
+#' @import spData
 #' @export
 
-plot_map <- function(data, variable) {
+plot_map <- function(data, variable, view = TRUE) {
+  if (!(view)) {
+    tmap_mode("plot")
+
+    bound_box = st_bbox(data)
+    bound_box[1] = bound_box[1] - 2
+    bound_box[2] = bound_box[2] - 2
+    bound_box[3] = bound_box[3] + 2
+    bound_box[4] = bound_box[4] + 2
+
+    map <- tm_shape(world, bbox = bound_box) +
+      tm_polygons() +
+      tm_shape(data[variable]) +
+      tm_lines(col = variable, lwd = 5) +
+      tm_layout(title = variable) +
+      tm_compass() +
+      tm_scale_bar()
+
+    return(map)
+  }
+
   tmap_mode("view")
+
   map <- tm_basemap(leaflet::providers$OpenTopoMap, alpha = 0.5) +
-    tm_shape(data[variable]) +
-    tm_lines(col = variable, lwd = 5)
+    tm_shape(lab_data[variable]) +
+    tm_lines(col = variable, lwd = 5) +
+    tm_layout(title = variable)
 
   return(map)
 }
+
+xcv = create_path(data)
+xcv
+vbn = plot_map(xcv, "baro_altitude", FALSE)
+vbn
+vbn
